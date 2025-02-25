423717d17609752789c57557980662d0d24430de
Martin Anton <119125486+martinxanton@users.noreply.github.com>
2025-02-24 21:39:30 -0500

    Actualizar configuración de entorno para incluir credenciales de base de datos

--- config/config.js
+++ config/config.js
@@ -2,4 +2,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const { JWT_SECRET_KEY, JWT_EXPIRES_IN, DB_URI, PORT } = process.env;
+export const { JWT_SECRET_KEY, JWT_EXPIRES_IN, PORT, DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
